893b62f2ecd78859dc09ab36a73715b84294cf24
meeramkhalid2 <meeram.khalid@aerodyne.group>
2024-06-25 17:22:22 +0500

    Add files via upload

---
+++ toggle-button/toggle-button.component.ts
@@ -0,0 +1,20 @@
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggle-button.component.html',
+  styleUrl: './toggle-button.component.css'
+})
+export class ToggleButtonComponent {
+
+  @Input() active: boolean=false;
+  @Output() stateChanged = new EventEmitter<boolean>(false);
+
+ 
+  toggleState():void {
+    this.active = !this.active;
+    this.stateChanged.emit(this.active); 
+  }
+}
